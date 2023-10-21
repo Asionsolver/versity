ee011b77bce64e11c8c97ae8a14eaab93ee307c4
asionsolver <asionsolver274@gmail.com>
2023-10-21 12:48:52 +0600

    some code delete in contributorA branch

--- hellow.js
+++ hellow.js
@@ -1,18 +1,8 @@
 var age = 18;
 
-var name = "Ashis Kumar Pal";
+var name = "Ashis";
 
 
 var templateString = `My name is ${name} and I am ${age} years old. I am ${age >= 18 ? 'an adult' : 'not an adult'}`;
 
 console.log(templateString);
-
-
-
-// var strings = `This is only string`;
-// console.log(strings);
-
-// console.log(name.padStart(20));
-// console.log(name.padStart(20, 'a'));
-// console.log(name.padEnd(20, 'a'));
-// console.log('s'.repeat(10))
